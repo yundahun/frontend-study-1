2b859c9a206922e9638edadf1120b4e72387c14a
geoblo <geoblo@naver.com>
2024-04-23 15:50:30 +0900

    up

--- 11_React/my-app/src/index.js
+++ 11_React/my-app/src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import JsxUse from './chapter3/3.4/JsxUse';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Root DOM Node에 리액트 컴포넌트를 렌더링 하도록 하는 함수(React 엘리먼트를 DOM 엘리먼트로 렌더링하는 역할)
+// 처음으로 렌더링할 컴포넌트를 지정하는데 App 컴포넌트가 기본적으로 들어가있음
 root.render(
   // <App />
 
