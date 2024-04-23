96d131f9b02d9dac51c8144852f48cf87b00e853
geoblo <geoblo@naver.com>
2024-04-23 16:50:52 +0900

    up

--- 11_React/my-app/src/chapter3/3.4/JsxUse.jsx
+++ 11_React/my-app/src/chapter3/3.4/JsxUse.jsx
@@ -19,22 +19,46 @@ function JsxUse() {
 
   // 2. 꼭 감싸져야만 하는 태그
   // 두개 이상의 태그는 무조건 하나의 태그로 감싸져있어야 함
-  const element = (
-    // 반드시 하나의 부모 태그로 감싸져야 함
-    // <div>
-    //   <div>안녕하세요.</div>
-    //   <div>안녕히계세요.</div>
-    // </div>
+  // const element = (
+  //   // 반드시 하나의 부모 태그로 감싸져야 함
+  //   // <div>
+  //   //   <div>안녕하세요.</div>
+  //   //   <div>안녕히계세요.</div>
+  //   // </div>
 
-    // 이렇게 단순히 감싸기 위하여 불필요한 div로 감싸는게 별로 좋지 않을 수 있음
-    // HTML 중첩 구조가 복잡해지며 그에 따른 스타일 설정을 할 때도 복잡해짐
-    // 이때 사용하는 것이 React Fragment
-    <React.Fragment>
-      <div>안녕하세요.</div>
-      <div>안녕히계세요.</div>
-    </React.Fragment>
-
+  //   // 이렇게 단순히 감싸기 위하여 불필요한 div로 감싸는게 별로 좋지 않을 수 있음
+  //   // HTML 중첩 구조가 복잡해지며 그에 따른 스타일 설정을 할 때도 복잡해짐
+  //   // 이때 사용하는 것이 React Fragment
+  //   // <React.Fragment>
+  //   //   <div>안녕하세요.</div>
+  //   //   <div>안녕히계세요.</div>
+  //   // </React.Fragment>
     
+  //   // React.Fragment는 생략 가능
+  //   <>
+  //     <div>안녕하세요.</div>
+  //     <div>안녕히계세요.</div>
+  //   </>
+  // );
+
+  // 3. JSX 안에 자바스크립트 값 또는 표현식 사용하기
+  // 자바스크립트 변수(또는 함수)를 사용할 때는 {} 사용
+  const name = 'react';
+
+  const user = {
+    firstName: 'Goni',
+    lastName: 'Kim'
+  };
+
+  const formatName = (user) => {
+    return `${user.firstName} ${user.lastName}`;
+  };
+
+  const element = (
+    <>
+      <div>{name} 배우기</div>
+      <h1>Hello, {formatName(user)}</h1>
+    </>
   );
 
   return element;
