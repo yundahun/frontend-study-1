8cec20305d45b6fa0ab4c168c8e7d2a920252335
geoblo <geoblo@naver.com>
2024-04-25 14:23:59 +0900

    up

---
+++ 11_React/my-app/src/chapter5/5.6/CommentEx.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function CommentEx(props) {
+  return (
+    <>
+      {/* 원본 */}
+      <div className="comment">
+        <div className="user-info">
+          <img className="avatar"
+            src={props.author.avatarUrl}
+            alt={props.author.name}
+          />
+          <div className="user-info-name">
+            {props.author.name}
+          </div>
+        </div>
+
+        <div className="comment-text">
+          {props.text}
+        </div>
+
+        <div className="comment-date">
+          {formatDate(props.date)}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default CommentEx;
