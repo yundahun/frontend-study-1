eec2f84d8d3e8efc00d44b187e59bd4bbb725561
geoblo <geoblo@naver.com>
2024-05-07 15:49:32 +0900

    up

---
+++ 11_React/my-post/src/App.js
@@ -0,0 +1,49 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+// POST앱 CRUD 만들기
+// C: Create(등록)
+// R: Read(목록, 상세보기)
+// U: Update(수정)
+// D: Delete(삭제)
+
+function App() {
+  // 서버에서 가져온 데이터라고 가정
+  const [posts, setPosts] = useState(['리액트 잘 쓰려면?', '자바스크립트 핵심 문법', 'CSS 스타일링 가이드']);
+
+  return (
+    <>
+      <header className="header--dark">
+        <h4>Gonilog</h4>
+        <nav>
+          <ul>
+            <li>트렌딩</li>
+            <li>최신</li>
+          </ul>
+        </nav>
+      </header>
+
+      <div className="inner">
+        {/* 포스트 목록 */}
+        <div className='list'>
+          <h4>{posts[0]}</h4>
+          <p>2023년 1월 20일</p>
+          <p>by goni.kim</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[1]}</h4>
+          <p>2023년 1월 20일</p>
+          <p>by goni.kim</p>
+        </div>
+        <div className='list'>
+          <h4>{posts[2]}</h4>
+          <p>2023년 1월 20일</p>
+          <p>by goni.kim</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
