dedb82c63b7562881c7d92ecf58df25a9428ed59
geoblo <geoblo@naver.com>
2024-05-08 15:48:55 +0900

    up

--- 11_React/my-post/src/App.js
+++ 11_React/my-post/src/App.js
@@ -12,9 +12,10 @@ import PostDetail from './components/PostDetail';
 function App() {
   // 서버에서 가져온 데이터라고 가정
   const [posts, setPosts] = useState(['리액트 잘 쓰려면?', '자바스크립트 핵심 문법', 'CSS 스타일링 가이드']);
-
   const [showPostDetail, setShowPostDetail] = useState(false);
   const [currentIndex, setCurrentIndex] = useState(null);
+  const [likeCount, setLikeCount] = useState([0, 0, 0]);
+  const [inputValue, setInputValue] = useState('');
 
   return (
     <>
@@ -58,13 +59,56 @@ function App() {
               <h4>{post}</h4>
               <p>2023년 1월 20일</p>
               <p>by goni.kim</p>
+
+              <hr />
+
+              <div className='toolbar'>
+                <span onClick={(e) => {
+                  // (버그 수정) 현재는 좋아요 버튼 누를 때 글 상세보기까지 같이 클릭됨!
+                  // 부모-자식 관계에 있을 때 이벤트 버블링이 일어남
+                  e.stopPropagation(); // 상위 요소로 퍼지는 이벤트 버블링을 막음
+
+                  const copyLikeCount = [...likeCount];
+                  copyLikeCount[index]++;
+                  setLikeCount(copyLikeCount);
+                }}>
+                  💗 {likeCount[index]}
+                </span>
+              </div>
             </div>
           );
         })}
 
+        {/* 포스트 등록하기 */}
+        {/* Quiz: input에 제목 입력 후 등록 버튼 누르면 맨 앞에 새로운 포스트 추가
+
+          1) input을 제어 컴포넌트로 만들어서 사용자가 입력한 값을 state로 저장해서 관리
+          2) 등록 버튼 클릭 시 posts 상태의 맨 앞에 새로운 데이터 추가
+        */}
+        <input type="text" value={inputValue} onChange={(e) => { setInputValue(e.target.value); }} />
+        <button type='button' onClick={() => {
+          // div 하나를 새로 생성 X
+          // posts state에 요소 하나 추가하면 자동으로 렌더링 O
+          // const copyPosts = [...posts];
+          // copyPosts.unshift(inputValue);
+
+          // 또는
+          const copyPosts = [inputValue, ...posts];
+          setPosts(copyPosts);
+
+          setInputValue('');
+
+          // (버그 수정) 포스트 하나 추가 시 좋아요 카운트도 같이 추가
+          const copyLikeCount = [0, ...likeCount];
+          setLikeCount(copyLikeCount);
+        }}>
+          포스트 등록
+        </button>
+
+
         {/* 포스트 상세보기 */}
         {/* Quiz: 조건부 렌더링 */}
-        {showPostDetail && <PostDetail posts={posts} currentIndex={currentIndex} />}
+        {showPostDetail && <PostDetail posts={posts} currentIndex={currentIndex} setPosts={setPosts} />}
       </div>
     </>
   );
--- 11_React/my-post/src/components/PostDetail.jsx
+++ 11_React/my-post/src/components/PostDetail.jsx
@@ -1,5 +1,5 @@
 function PostDetail(props) {
-  const { posts, currentIndex } = props;
+  const { posts, currentIndex, setPosts } = props;
 
   return (
     <div className="detail">
@@ -7,6 +7,27 @@ function PostDetail(props) {
       <p>날짜: 2023년 1월 20일</p>
       <p>작성자: goni.kim</p>
       <p>... 상세내용 ...</p>
+
+      {/* 간단한 포스트 수정하기 */}
+      <button type="button"
+        onClick={() => {
+          // 배열이나 객체의 state를 변경하는 법
+          // 새로운 배열 또는 객체를 만들어 변경하고 set함수에 넣어줘야 함
+          const copyPosts = [...posts]; // 배열의 복사본 만들기
+          copyPosts[currentIndex] = `${copyPosts[currentIndex]} - 수정됨`;
+          setPosts(copyPosts);
+
+          posts[currentIndex] = 'asdasda';
+
+          // state 변경할 때 알아야 할 것!
+          // 1. state 변경 함수의 특징
+          // 기존 state가 신규 state랑 같은 경우, 변경 안함
+          // 2. 배열/객체의 특징
+          // 변수에 주소(참조)값이 저장됨
+        }}
+      >
+        수정하기
+      </button>
     </div>
   );
 };
