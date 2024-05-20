0f5941bb799c3dd3cc848f92f5721577bb9eb4e2
geoblo <geoblo@naver.com>
2024-05-20 16:36:24 +0900

    up

--- 11_React/my-blog/src/component/page/PostViewPage.jsx
+++ 11_React/my-blog/src/component/page/PostViewPage.jsx
@@ -1,2 +1,47 @@
+const Wrapper = styled.div`
+  padding: 16px;
+  width: calc(100% - 32px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
+const Container = styled.div`
+  width: 100%;
+  max-width: 720px;
 
+  & > *:not(:last-child) {
+    margin-bottom: 16px;
+  }
+`;
+
+const PostContainer = styled.div`
+  padding: 8px 16px;
+  border: 1px solid grey;
+  border-radius: 8px;
+`;
+
+const TitleText = styled.p`
+  font-size: 28px;
+  font-weight: 500;
+`;
+
+const ContentText = styled.p`
+  font-size: 20px;
+  line-height: 32px;
+`;
+
+const CommentLabel = styled.p`
+  font-size: 16px;
+  font-weight: 600;
+`;
+
+function PostViewPage() {
+  return (
+    <>
+    </>
+  );
+};
+
+export default PostViewPage;
